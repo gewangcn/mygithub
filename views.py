c19878d221ce98f6ee11b2a4d59a7dd36b97058b
gewangcn <493176964@qq.com>
2020-11-19 00:40:03 -0800

    创建login视图

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcut import redirect
 
 def index(request):
     return HttpResponse('hello github')
+
+def login(request):
+    return redirct('/index')
